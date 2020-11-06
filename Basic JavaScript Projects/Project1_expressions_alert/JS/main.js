0500238aa4704cd80882cc9c3abeb2fa1ccc811e
tenchamp31 <ygandhi191@gmail.com>
2020-11-06 11:53:51 -0500

    step 59

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+var Cars= "Automobiles", Ford= "American", Mercades= "German", Jaguar= "United Kingdom", Tata= "India"; // Assigning variables 
+var Cars= Cars.fontcolor("green"); //Assigning font color
+var Ford= Ford.fontcolor("blue");
+var Mercades= Mercades.fontcolor("red");
+var Jaguar= Jaguar.fontcolor("pink");
+var Tata= Tata.fontcolor("orange");
+
+
+
+
+document.write("\"Programming today is a race between software engineers striving to build bigger and better idiot-proof programs,"
++ "and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.\""
++ "-Rick Cook");
+
+document.write(9-8);
+
+alert("step 59");
