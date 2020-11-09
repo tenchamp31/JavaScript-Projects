44c6108b6a035889459a844c2f4f3e27ede894e6
tenchamp31 <ygandhi191@gmail.com>
2020-11-09 10:31:33 -0500

    fixing

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -6,7 +6,7 @@ var Jaguar= Jaguar.fontcolor("pink");
 var Tata= Tata.fontcolor("orange");
 
 
-
+document.write(Ford + Mercades);  //Concatenating
 
 document.write("\"Programming today is a race between software engineers striving to build bigger and better idiot-proof programs,"
 + "and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.\""
