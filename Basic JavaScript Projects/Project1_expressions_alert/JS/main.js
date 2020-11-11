c1560d3bd8e34042cd774729220026426467d7c0
tenchamp31 <ygandhi191@gmail.com>
2020-11-11 15:44:19 -0500

    Step

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,17 +1,5 @@
-var Cars= "Automobiles", Ford= "American", Mercades= "German", Jaguar= "United Kingdom", Tata= "India"; // Assigning variables 
-var Cars= Cars.fontcolor("green"); //Assigning font color
-var Ford= Ford.fontcolor("blue");
-var Mercades= Mercades.fontcolor("red");
-var Jaguar= Jaguar.fontcolor("pink");
-var Tata= Tata.fontcolor("orange");
-
-
-document.write(Ford + Mercades);  //Concatenating
-
-document.write("\"Programming today is a race between software engineers striving to build bigger and better idiot-proof programs,"
-+ "and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.\""
-+ "-Rick Cook");
-
-document.write(9-8);
-
-alert("step 59");
+function myFunction() {
+    var sentence = "I am learning";
+sentence += "a lot from this book!";
+document.getElementById("Concatenate").innerHTML = sentence;
+}
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  var sentence = "Chicago";
+  sentence += " has one of the best skylines in the United States";
+  document.getElementById("Concatenate").innerHTML = sentence;
+}
