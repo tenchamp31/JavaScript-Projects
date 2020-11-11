cdf4a3e23a6ab0da10593731009c92aaf13a9bfc
tenchamp31 <ygandhi191@gmail.com>
2020-11-11 15:46:42 -0500

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
 function myFunction() {
   var sentence = "Chicago";
-  sentence += " has one of the best skylines in the United States";
-  document.getElementById("Concatenate").innerHTML = sentence;
+  sentence += " has one of the best skylines in the United States"; // Adding paragraph
+  document.getElementById("Concatenate").innerHTML = sentence; // Modyfing content within HTML
 }
