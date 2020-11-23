60138e620a89ace6921efb72597e5019a950fad8
tenchamp31 <ygandhi191@gmail.com>
2020-11-23 13:02:32 -0500

    Update main.js

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,18 +1,18 @@
-document.write(typeof 15);
+document.write(typeof 15); // Tyoe of Operator
 
-document.write("15"+6);
+document.write("15"+6); // String + number
 
-document.write(16 == 16);
+document.write(16 == 16); // Comparing values
 
 g= "Italy";
 h= "France";
 document.write(g === h);
 
-document.write( 12 >3 && 78 >34);
+document.write( 12 >3 && 78 >34); // And
 
-document.write (13> 38 || 65>23);
+document.write (13> 38 || 65>23); // OR
 
-function not_Function () {
+function not_Function () { // Not operator
     document.getElementById("Not").innerHTML = !(20 > 10);
 }
 
